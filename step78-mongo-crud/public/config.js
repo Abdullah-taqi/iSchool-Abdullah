365ebbfed0b5c411bae12725455484de3409b411
Abdullah Taqi <ataqi99@gmail.com>
2022-03-31 10:56:56 +0530

    Delete step78-mongo-crud directory

--- step78-mongo-crud/public/config.js
+++
@@ -1,6 +0,0 @@
-{
-    "cloudDB" :"mongodb+srv://Ineffable:Zxcvb99@mycluster.f2jpd.mongodb.net/onlinedb?retryWrites=true&w=majority",
-    "localDB" : "mongodb://localhost:27017//iSchoolDB",
-    "port" : "5050",
-    "host" : "localhost"
-}
