ef478a6078e7e49eb06ba16b9f7b3152e791d39f
Abdullah Taqi <ataqi99@gmail.com>
2022-04-16 02:28:42 +0530

    Laundry Management System
    Working on further updates

---
+++ Laundry Management App/MEAN-App-main/backend/models/employee.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Employee = mongoose.model('Employee', {
+    name : {type:String},
+    address : {type:String},
+    quantity : {type:String},
+    process : {type:String},
+});
+
+module.exports = Employee;
